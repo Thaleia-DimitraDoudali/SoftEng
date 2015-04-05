76c0f3e3c75b806cc3da6cee171f2570a2d70450
Thaleia-Dimitra Doudali <doudali@gmail.com>
2015-04-05 17:02:39 +0300

    class for the client to communicate to the database - registerToDB() function

---
+++ SipCommunicator-Fall05/net/java/sip/communicator/db/RegisterDB.java
@@ -0,0 +1,47 @@
+package net.java.sip.communicator.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RegisterDB {
+	String connectionURL = "jdbc:mysql://localhost:3306/softeng";
+	Connection connection = null;
+	Statement statement = null;
+	String dbuser = "root";
+	String dbpass = "";	
+	
+	//Constructor
+	public RegisterDB() {
+		register();
+	}
+	
+	public void register() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(connectionURL, dbuser, dbpass);
+		} catch (SQLException | ClassNotFoundException e) {
+			//Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void registerToDB(String username, String passwd, String email, String creditCard) {
+		
+		try {
+			if (connection == null)
+				register();
+			statement = connection.createStatement();
+			String sql = String
+					.format("INSERT INTO users set username = '%s', email = '%s', password = '%s', creditCard = '%s'",
+							username, email, passwd, creditCard);
+			System.out.println(sql);
+			statement.execute(sql);
+		} catch (SQLException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+}
